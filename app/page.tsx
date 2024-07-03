09fc619ca2a16b77ffc1c463f67734ff37180e79
kengon <kengon56@gmail.com>
2024-07-03 09:00:44 +0700

    setup supa-authen

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+
+export default function Home() {
+  const setNewView = async () => {
+    console.log("SEt new view");
+  };
+
+  setNewView();
+
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      hello
+    </main>
+  );
+}
